c0081bae6eb19a7549133f298c6d60f8ccc95820
Ashi <500052000@stu.upes.ac.in>
2017-12-31 13:44:14 +0530

    new activity after registration

---
+++ app/src/main/java/com/example/ashi/a1myapplication/AfterRegistration.java
@@ -0,0 +1,13 @@
+package com.example.ashi.a1myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AfterRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_registration);
+    }
+}
--- app/src/main/java/com/example/ashi/a1myapplication/Register.java
+++ app/src/main/java/com/example/ashi/a1myapplication/Register.java
@@ -1,6 +1,7 @@
 package com.example.ashi.a1myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -25,6 +26,8 @@ public class Register extends Fragment {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),AfterRegistration.class);
+                startActivity(intent);
 
             }
         });
